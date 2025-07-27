0ac1c7bab0697abb3a16830cfe191fbeea9487a1
Bury, Hogan & Associates <buryhogan@gmail.com>
2025-07-27 00:15:16 -0700

    Update MaxMartin.sh

--- MaxMartin.sh
+++ MaxMartin.sh
@@ -3,9 +3,9 @@
 # Intel Macs from Homebrew
 #path=/usr/local/Cellar/sendmidi
 # Apple Silicon Macs from Homebrew
-path=/opt/homebrew/bin/sendmidi
+#path=/opt/homebrew/bin/sendmidi
 # Binary installs
-#path=/usr/local/bin/sendmidi
+path=/usr/local/bin/sendmidi
 #
 cmd1="dev IAC Driver Bus 1 ch 5 cc 1 127" # call "Fade Out" scene
 cmd2="dev IAC Driver Bus 1 ch 4 cc 4 127" # LED scene 2
