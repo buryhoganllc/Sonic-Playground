a31f0b017b35a262de6fc4947747c817aadd793f
Bury, Hogan & Associates <buryhogan@gmail.com>
2025-07-27 00:14:32 -0700

    Update and rename sendmidi Events.sh to Nighttime_Mode.sh

---
+++ Nighttime_Mode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# uncomment the correct path to your sendmidi install location:
+# Intel Macs from Homebrew
+#path=/usr/local/Cellar/sendmidi
+# Apple Silicon Macs from Homebrew
+#path=/opt/homebrew/bin/sendmidi
+# Binary installs
+path=/usr/local/bin/sendmidi
+#
+# Example: set output level of North/South surrounds to -12dB, value scaling range (-40dB to -2.5dB)
+cmd="dev IAC Driver Bus 1 ch 5 cc 50 79" 
+#
+midiEvent="$path $cmd"
+#
+eval "$midiEvent"
+exit 0;
+
